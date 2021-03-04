a7c5499c0065549dbb8192130505f9d53288c1bf
ROG-PC <ROG-PC@192.168.1.67>
2021-03-04 01:39:04 +0000

    # WARNING: head commit changed in the meantime
    Test Classes

---
+++ src/test/AllTests.java
@@ -0,0 +1,21 @@
+package com.todotxt.todotxttouch.util;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({})
+public class AllTests {
+	
+	import java.util.EmptyStackException;
+	import java.util.Stack;
+
+	private class JunitTesting extends copyFile {
+
+		@Test
+	    @FileSource(file = newFile)
+	    void testFileExists(newFile.exists())
+	    }
+	}
+}
