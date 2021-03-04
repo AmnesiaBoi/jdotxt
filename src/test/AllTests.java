51ea0e390d7a723c2eb27360e1b7624aab086cf8
ROG-PC <ROG-PC@192.168.1.67>
2021-03-04 20:12:43 +0000

    Added guide parameters

--- src/test/AllTests.java
+++ src/test/AllTests.java
@@ -19,3 +19,46 @@ public class AllTests {
 	    }
 	}
 }
+
+
+/*Category Partition Method
+
+grep - search a file for a X pattern
+
+grep <pattern> <filename>
+
+
+guide:
+http://209.242.1.146/docs/TestPatternCategoryPartition.pdf
+
+Parameters:
+	Pattern size:
+	empty 										[property Empty]
+	single character 							[property NonEmpty]
+	many character 								[property NonEmpty]
+	longer than any line in the file 			[property NonEmpty]
+Quoting:
+	pattern is quoted 							[property Quoted1
+	pattern is not quoted 						[if NonEmpty]
+	pattern is improperly quoted 				[if NonEmpty]
+Embeddedblanks:
+	no embedded blank 							[if NonEmpty]
+	one embedded blank 							[if NonEmpty and Quoted1
+	several embedded blanks 					[if NonEmpty and Quoted]
+Embeddedquotes:
+	no embedded quotes 							[if NonEmpty]
+	one embedded quote 							[if NonEmpty]
+	several embedded quotes 					[if NonEmpty]
+Filename:
+	good file name
+	no file with this name
+	omitted
+Environments:
+	Number of occurrences ofpatterninfile:
+		none									[if NonEmpty]
+		exactly one 							[if NonEmpty] [property Match]
+		more than one 							[if NonEmpty] [property Match]
+	Pattern occurrencesontargetline:
+		# assumes line contains the pattern
+		one 									[if Match]
+		more than one 							[if Match]
