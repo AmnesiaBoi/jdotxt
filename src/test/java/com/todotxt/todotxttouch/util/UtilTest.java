3c49c35bd03b3332868eec5997ac6bedf7ed2be4
Diogo Barros <fc51959@alunos.fc.ul.pt>
2021-03-08 07:32:54 +0000

    Changed test combinations

--- src/test/java/com/todotxt/todotxttouch/util/UtilTest.java
+++ src/test/java/com/todotxt/todotxttouch/util/UtilTest.java
@@ -1,7 +1,6 @@
 package com.todotxt.todotxttouch.util;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -11,40 +10,57 @@ import org.junit.Test;
 
 public class UtilTest 
 {
-	@Test 
-	public void split1() {
-		List<String> expected = Arrays.asList("abc", "def");
-		List<String> actual = Util.split(null, ";");
-
-		assertEquals(expected, actual);
+	
+	/*
+	 * Util.split(String s, String delimeter)
+	 * 
+	 * Variations:
+	 * 		s        : null, "", no delim, with delim
+	 * 		delimeter: null, "", delim
+	 * 
+	 * Combinations:   (R for Redundant)
+	 * 		#  , s         , delimeter, expected
+	 * 
+	 * 		1  , null      , delim    , NullPointerException
+	 * 		2  , no delim  , null     , NullPointerException
+	 * 
+	 *      3  , ""        , ""       , []
+	 *      4  , ""        , delim    , []
+	 *      5  , no delim  , ""       , Arrays.asList(s.split(""))
+	 *      6  , no delim  , delim    , [s]
+	 *      7  , with delim, ""       , Arrays.asList(s.split(""))
+	 *      8  , with delim, delim    , Arrays.asList(s.split(delimeter))
+	 *      
+	 */
+	
+	@Test(expected=NullPointerException.class)
+	public void splitTest1() {
+		Util.split(null, ";");
 	}
 
-	@Test
-	public void split2() {
-		List<String> expected = Arrays.asList("abc", "def");
-		List<String> actual = Util.split("", null);
-
-		assertEquals(expected, actual);
+	@Test(expected=NullPointerException.class)
+	public void splitTest2() {
+		Util.split("abcdef", null);
 	}
 
 	@Test 
-	public void split3() {
-		List<String> expected = new ArrayList<String>();
+	public void splitTest3() {
+		List<String> expected = new ArrayList<>();
 		List<String> actual = Util.split("", "");
 
 		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void split4() {
-		List<String> expected = new ArrayList<String>();
+	public void splitTest4() {
+		List<String> expected = new ArrayList<>();
 		List<String> actual = Util.split("", ";");
 
 		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void split5() {
+	public void splitTest5() {
 		List<String> expected = Arrays.asList("a", "b", "c", "d", "e", "f");
 		List<String> actual = Util.split("abcdef", "");
 
@@ -52,7 +68,7 @@ public class UtilTest
 	}
 
 	@Test
-	public void split6() {
+	public void splitTest6() {
 		List<String> expected = Arrays.asList("abcdef");
 		List<String> actual = Util.split("abcdef", ";");
 		
@@ -60,75 +76,56 @@ public class UtilTest
 	}
 
 	@Test 
-	public void split7() {
-		List<String> expected = Arrays.asList(";", "a", "b", "c", "d", "e", "f");
-		List<String> actual = Util.split(";abcdef", "");
-		
-		assertEquals(expected, actual);
-	}
-
-	@Test 
-	public void split8() {
-		List<String> expected = Arrays.asList("","abcdef");
-		List<String> actual = Util.split(";abcdef", ";");
-		
-		assertEquals(expected, actual);
-	}
-
-	@Test 
-	public void split9() {
-		List<String> expected = Arrays.asList("abcdef");
-		List<String> actual = Util.split("abcdef;", ";");
-		
-		assertEquals(expected, actual);
-	}
-
-	@Test 
-	public void split10() {
-		List<String> expected = Arrays.asList("abcdef;");
-		List<String> actual = Util.split("abcdef;", ";");
-		
-		assertEquals(expected, actual);
-	}
-
-	@Test 
-	public void split11() {
-		List<String> expected = Arrays.asList("abc;def");
+	public void splitTest7() {
+		List<String> expected = Arrays.asList("a", "b", "c", ";", "d", "e", "f");
 		List<String> actual = Util.split("abc;def", "");
 		
 		assertEquals(expected, actual);
 	}
 
 	@Test 
-	
-	public void split12() {
+	public void splitTest8() {
 		List<String> expected = Arrays.asList("abc", "def");
 		List<String> actual = Util.split("abc;def", ";");
-
+		
 		assertEquals(expected, actual);
 	}
 
-	@Test 
-	public void join1() {
+	
+	/*
+	 * Util.join(Collection<?> s, String delimiter)
+	 * 
+	 * Variations:
+	 * 		s        : null, empty, some
+	 * 		delimiter: null, "", delim
+	 * 
+	 * Combinations:   (R for Redundant)
+	 * 		#  , s    , delimiter, expected
+	 * 
+	 *      1  , null , delim    , NullPointerException
+	 *      2  , some , null     , NullPointerException
+	 *      
+	 *      3  , empty, ""       , ""
+	 *      4  , empty, delim    , ""
+	 *      5  , some , ""       , some[0] + some[1] ..
+	 *      6  , some , delim    , some[0] + delim + some[1] ..
+	 */
+	
+	@Test(expected=NullPointerException.class)
+	public void joinTest1() {
 		List<String> original = null;
-		String expected = null;
-		String actual = Util.join(original, ",");
-
-		assertEquals(expected, actual);
+		Util.join(original, ",");
 	}
 
-	@Test
-	public void join2() {
+	@Test(expected=NullPointerException.class)
+	public void joinTest2() {
 		List<String> original = Arrays.asList("abc", "def");
-		String expected = "abc,def";
-		String actual = Util.join(original, null);
-
-		assertEquals(expected, actual);
+		Util.join(original, null);
 	}
 
 	@Test
-	public void join3() {
-		List<String> original = Arrays.asList("");
+	public void joinTest3() {
+		List<String> original = Arrays.asList();
 		String expected = "";
 		String actual = Util.join(original, "");
 
@@ -136,8 +133,8 @@ public class UtilTest
 	}
 
 	@Test
-	public void join4() {
-		List<String> original = Arrays.asList("");
+	public void joinTest4() {
+		List<String> original = Arrays.asList();
 		String expected = "";
 		String actual = Util.join(original, ",");
 
@@ -145,16 +142,16 @@ public class UtilTest
 	}
 
 	@Test
-	public void join5() {
+	public void joinTest5() {
 		List<String> original = Arrays.asList("abc", "def");
-		String expected = "";
+		String expected = "abcdef";
 		String actual = Util.join(original, "");
 
 		assertEquals(expected, actual);
 	}
 
 	@Test
-	public void join6() {
+	public void joinTest6() {
 		List<String> original = Arrays.asList("abc", "def");
 		String expected = "abc,def";
 		String actual = Util.join(original, ",");
