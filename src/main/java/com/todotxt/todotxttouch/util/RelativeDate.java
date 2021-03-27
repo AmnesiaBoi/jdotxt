3249897db5943719b6aea281c3e6d4b768042fee
Diogo Barros <fc51959@alunos.fc.ul.pt>
2021-03-27 21:13:56 +0000

    Cleanup

--- src/main/java/com/todotxt/todotxttouch/util/RelativeDate.java
+++ src/main/java/com/todotxt/todotxttouch/util/RelativeDate.java
@@ -8,7 +8,6 @@ package com.todotxt.todotxttouch.util;
 import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -19,13 +18,6 @@ public class RelativeDate {
 
 	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 	//private static Context context = TodoApplication.getAppContetxt();
-
-	// Doesn't handle leap year, etc, but we don't need to be very
-	// accurate. This is just for human readable date displays.
-	private static final long SECOND = 1000; //milliseconds
-	private static final long HOUR = 3600 * SECOND;
-	private static final long DAY = 24 * HOUR;
-	private static final long YEAR = 365 * DAY;
 
 	/**
 	 * This method returns a String representing the relative date by comparing
@@ -61,8 +53,6 @@ public class RelativeDate {
 		// Convertint to a more recent and flexible Java object
 		LocalDateTime ldt1 = LocalDateTime.ofInstant(d1.toInstant(), zoneId);
 		LocalDateTime ldt2 = LocalDateTime.ofInstant(d2.toInstant(), zoneId);
-
-		long diff = ldt1.until(ldt2, ChronoUnit.MILLIS);
 		
 		
 		if (Math.abs(ldt1.getYear() - ldt2.getYear()) >= 1) {
